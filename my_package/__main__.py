9b24e2983e44fd29533bacc5739edaf352a1c98a
SmritiSah <smritisah863@gmail.com>
2025-03-25 16:14:53 +0530

    commit

--- my_package/__main__.py
+++ my_package/__main__.py
@@ -2,38 +2,48 @@ import typer
 from rich.console import Console
 from rich.table import Table
 from my_package.model import Todo
-from my_package.database import get_all_todos, delete_todo, insert_todo, complete_todo, update_todo
+from my_package.database import (
+    get_all_todos,
+    delete_todo,
+    insert_todo,
+    complete_todo,
+    update_todo,
+)
 
 console = Console()
 
 app = typer.Typer()
 
 
-@app.command(short_help='adds an item')
+@app.command(short_help="adds an item")
 def add(task: str, category: str):
     typer.echo(f"adding {task}, {category}")
     todo = Todo(task, category)
     insert_todo(todo)
     show()
 
+
 @app.command()
 def delete(position: int):
     typer.echo(f"deleting {position}")
     # indices in UI begin at 1, but in database at 0
-    delete_todo(position-1)
+    delete_todo(position - 1)
     show()
+
 
 @app.command()
 def update(position: int, task: str = None, category: str = None):
     typer.echo(f"updating {position}")
-    update_todo(position-1, task, category)
+    update_todo(position - 1, task, category)
     show()
+
 
 @app.command()
 def complete(position: int):
     typer.echo(f"complete {position}")
-    complete_todo(position-1)
+    complete_todo(position - 1)
     show()
+
 
 @app.command()
 def show():
@@ -47,15 +57,15 @@ def show():
     table.add_column("Done", min_width=12, justify="right")
 
     def get_category_color(category):
-        COLORS = {'Learn': 'cyan', 'YouTube': 'red', 'Sports': 'cyan', 'Study': 'green'}
+        COLORS = {"Learn": "cyan", "YouTube": "red", "Sports": "cyan", "Study": "green"}
         if category in COLORS:
             return COLORS[category]
-        return 'white'
+        return "white"
 
     for idx, task in enumerate(tasks, start=1):
         c = get_category_color(task.category)
-        is_done_str = '✅' if task.status == 2 else '❌'
-        table.add_row(str(idx), task.task, f'[{c}]{task.category}[/{c}]', is_done_str)
+        is_done_str = "✅" if task.status == 2 else "❌"
+        table.add_row(str(idx), task.task, f"[{c}]{task.category}[/{c}]", is_done_str)
     console.print(table)
 
 
